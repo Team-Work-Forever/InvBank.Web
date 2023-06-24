485579bef5c17d00b533a71e9a25681ed270aa37
CavasCallahan <dassuncao1234@gmail.com>
2023-06-24 19:12:34 +0100

    fix: all fund crud

--- Helper/EndPoints/FundEndPoint.cs
+++ Helper/EndPoints/FundEndPoint.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using ErrorOr;
 using InvBank.Web.Contracts;
+using InvBank.Web.Contracts.Account;
 using InvBank.Web.Contracts.Fund;
 
 namespace InvBank.Web.Helper.EndPoints;
@@ -9,6 +10,34 @@ public class FundEndPoint : BaseEndPoint
 {
     public FundEndPoint(AuthApiHelper apiHelper) : base(apiHelper)
     {
+    }
+
+    public async Task<ErrorOr<FundResponse>> GetFund(Guid fundId)
+    {
+        return await MakeRequest<FundResponse>(
+            () => _apiHelper.DoGet($"/funds?fundId={fundId}")
+        );
+    }
+
+    public async Task<ErrorOr<IEnumerable<FundResponse>>> GetAllFunds()
+    {
+        return await MakeRequest<IEnumerable<FundResponse>>(
+            () => _apiHelper.DoGet($"/funds/all")
+        );
+    }
+
+    public async Task<ErrorOr<FundResponse>> AssociateAccountToFund(AssociateAccountToFundRequest request)
+    {
+        return await MakeRequest<FundResponse>(
+            () => _apiHelper.DoPost($"/funds/associate", JsonSerializer.Serialize(request))
+        );
+    }
+
+    public async Task<ErrorOr<SimpleResponse>> InvestOnFund(string iban, Guid fundId, MakeTransferRequest request)
+    {
+        return await MakeRequest<SimpleResponse>(
+            () => _apiHelper.DoPost($"/funds/invest?iban={iban}&fundId={fundId}", JsonSerializer.Serialize(request))
+        );
     }
 
     public async Task<ErrorOr<SimpleResponse>> CreateFund(CreateFundRequest request)
@@ -21,7 +50,7 @@ public class FundEndPoint : BaseEndPoint
     public async Task<ErrorOr<IEnumerable<FundResponse>>> GetInvestFundOfAccount(string accountIban)
     {
         return await MakeRequest<IEnumerable<FundResponse>>(
-            () => _apiHelper.DoGet($"/funds?iban={accountIban}")
+            () => _apiHelper.DoGet($"/funds/account?iban={accountIban}")
         );
     }
 
